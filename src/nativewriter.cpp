98a085e104f60b33a004fad373b1f61060c18fc9
Mike <upstreammuse@users.noreply.github.com>
2018-12-18 19:50:07 -0600

    Finished initial cut of native file writer

--- src/nativewriter.cpp
+++ src/nativewriter.cpp
@@ -53,8 +53,83 @@ void NativeWriter::processItem(LedgerComment const& comment)
 void NativeWriter::processItem(LedgerTransaction const& transaction)
 {
    breakBetween();
-   std::cout << qPrintable(transaction.date().toString(Qt::SystemLocaleShortDate))
-             << std::endl;
+   std::cout << qPrintable(transaction.date().toString(Qt::SystemLocaleShortDate));
+   if (transaction.cleared())
+   {
+      std::cout << " * ";
+   }
+   else
+   {
+      std::cout << " ! ";
+   }
+   std::cout << qPrintable(transaction.account()) << "  ";
+   int numNotes = transaction.hasNote() ? 1 : 0;
+   foreach (LedgerTransactionEntry const& entry, transaction.entries())
+   {
+      if (entry.hasNote())
+      {
+         numNotes++;
+      }
+   }
+   if (transaction.entries().size() > 1 || numNotes > 1)
+   {
+      std::cout << qPrintable(transaction.amount().toString());
+      if (transaction.hasBalance())
+      {
+         std::cout << " = " << qPrintable(transaction.balance().toString());
+      }
+      if (transaction.hasNote())
+      {
+         std::cout << " ;" << qPrintable(transaction.note());
+      }
+      std::cout << std::endl;
+      foreach (LedgerTransactionEntry const& entry, transaction.entries())
+      {
+         std::cout << "   ";
+         if (entry.transfer())
+         {
+            std::cout << "@";
+         }
+         std::cout << qPrintable(entry.payee()) << "  ";
+         if (entry.hasCategory())
+         {
+            std::cout << qPrintable(entry.category()) << "  ";
+         }
+         std::cout << qPrintable(entry.amount().toString());
+         if (entry.hasNote())
+         {
+            std::cout << " ;" << qPrintable(entry.note());
+         }
+         std::cout << std::endl;
+      }
+   }
+   else
+   {
+      LedgerTransactionEntry entry(transaction.entries()[0]);
+      if (entry.transfer())
+      {
+         std::cout << "@";
+      }
+      std::cout << qPrintable(entry.payee());
+      if (entry.hasCategory())
+      {
+         std::cout << "  " << qPrintable(entry.category());
+      }
+      std::cout << "  " << qPrintable(transaction.amount().toString());
+      if (transaction.hasBalance())
+      {
+         std::cout << " = " << qPrintable(transaction.balance().toString());
+      }
+      if (transaction.hasNote())
+      {
+         std::cout << " ;" << qPrintable(transaction.note());
+      }
+      else if (entry.hasNote())
+      {
+         std::cout << " ;" << qPrintable(entry.note());
+      }
+      std::cout << std::endl;
+   }
 }
 
 void NativeWriter::stop()
