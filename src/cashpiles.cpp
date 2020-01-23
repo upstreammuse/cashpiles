fe6a19be175aa75acb64eb5a0d8eeb334800b532
Michael <upstreammuse@users.noreply.github.com>
2020-01-22 21:46:22 -0600

    Removing todos that are covered by issue #1

--- src/cashpiles.cpp
+++ src/cashpiles.cpp
@@ -14,8 +14,6 @@
 #include "iptransferbalancer.h"
 #include "ledger.h"
 
-// TODO if a category isn't a goal, then future transactions for it are ignored, so there is no check or prompt to save for them
-
 // TODO need to add support for withholding category so income taxes don't break routine expense calculations
 
 // TODO add validation to make sure categories don't exist in multiple places (two different types for the same name)
