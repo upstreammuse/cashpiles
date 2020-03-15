d014d6a3e2edcc50fb1c76a1085b5a4af9cad211
Michael <upstreammuse@users.noreply.github.com>
2020-03-15 14:55:22 -0500

    Removed qdate from main methods

--- src/date.h
+++ src/date.h
@@ -1,15 +1,20 @@
 #pragma once
 
+#include <string>
+
 class QDate;
 
 class Date
 {
 public:
+   static Date currentDate();
    static Date fromQDate(QDate const& date);
+   static Date fromString(std::string const& date, std::string const& format);
 
 public:
    long long int daysTo(Date const& other) const;
    bool isNull() const;
+   bool isValid() const;
    QDate toQDate() const;
    bool operator==(Date const& other) const;
 
