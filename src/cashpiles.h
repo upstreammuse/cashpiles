92ab95d203b44249518bb4a0c6797fd38f52a800
Michael <upstreammuse@users.noreply.github.com>
2020-08-21 22:19:12 -0500

    Cleaning up the cashpiles 'main' header

--- src/cashpiles.h
+++ src/cashpiles.h
@@ -12,10 +12,6 @@ class Date;
 [[noreturn]] void die(std::string const& fileName, size_t lineNum,
                       char const* message);
 
-void processArguments(bool& convertYnab, std::string& dateFormat,
-                      std::string& inFileName, std::string& outFileName,
-                      std::string& reportDir, int argc, char** argv);
-
 void warn(std::string const& message);
 
 void warn(std::string const& fileName, size_t lineNum,
