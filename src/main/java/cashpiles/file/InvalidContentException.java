5b0fd3667e7d133fefdabd6acb7d88934d1ad27c
upstreammuse <upstreammuse@users.noreply.github.com>
2021-07-11 15:14:49 -0500

    Avoid constructing ledger exceptions outside the package

--- src/main/java/cashpiles/file/InvalidContentException.java
+++ src/main/java/cashpiles/file/InvalidContentException.java
@@ -3,7 +3,7 @@ package cashpiles.file;
 @SuppressWarnings("serial")
 public class InvalidContentException extends Exception {
 
-	public InvalidContentException(String fileName, int lineNumber, String line) {
+	InvalidContentException(String fileName, int lineNumber, String line) {
 		super("Invalid line '" + line + "' in file '" + fileName + "', line " + lineNumber);
 	}
 
--- src/main/java/cashpiles/file/UnknownIdentifierException.java
+++ src/main/java/cashpiles/file/UnknownIdentifierException.java
@@ -5,11 +5,11 @@ import cashpiles.file.LedgerReader.IdentifierType;
 @SuppressWarnings("serial")
 public class UnknownIdentifierException extends Exception {
 
-	public UnknownIdentifierException(String identifier) {
+	UnknownIdentifierException(String identifier) {
 		super("Unknown identifier '" + identifier + "'");
 	}
 
-	public UnknownIdentifierException(String identifier, IdentifierType type) {
+	UnknownIdentifierException(String identifier, IdentifierType type) {
 		super("Unknown identifier '" + identifier + "' of type " + type);
 	}
 
