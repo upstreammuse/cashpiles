8e18a66bcde9a6d20f7fcebc3da39b5328bf4614
upstreammuse <upstreammuse@users.noreply.github.com>
2021-07-11 15:14:49 -0500

    Avoid constructing ledger exceptions outside the package

--- src/main/java/cashpiles/file/IdentifierMismatchException.java
+++ src/main/java/cashpiles/file/IdentifierMismatchException.java
@@ -5,7 +5,7 @@ import cashpiles.file.LedgerReader.IdentifierType;
 @SuppressWarnings("serial")
 public class IdentifierMismatchException extends Exception {
 
-	public IdentifierMismatchException(String identifier, IdentifierType type, IdentifierType expected) {
+	IdentifierMismatchException(String identifier, IdentifierType type, IdentifierType expected) {
 		super(expected + " '" + identifier + "' used as " + type);
 	}
 
