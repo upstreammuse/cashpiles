f6736bee1521fe8e11376a7ec66744037c200627
upstreammuse <upstreammuse@users.noreply.github.com>
2021-10-11 17:20:11 -0500

    Made LedgerItem a value class

--- src/main/java/cashpiles/ledger/LedgerItem.java
+++ src/main/java/cashpiles/ledger/LedgerItem.java
@@ -2,14 +2,28 @@ package cashpiles.ledger;
 
 public abstract class LedgerItem {
 
-	protected final String comment;
-	protected final String fileName;
-	protected final int lineNumber;
+	private final String comment;
+	private final String fileName;
+	private final int lineNumber;
 
 	public LedgerItem(String fileName, int lineNumber, String comment) {
 		this.comment = comment;
 		this.fileName = fileName;
 		this.lineNumber = lineNumber;
+	}
+
+	public LedgerItem(LedgerItem other) {
+		comment = other.comment;
+		fileName = other.fileName;
+		lineNumber = other.lineNumber;
+	}
+
+	public String comment() {
+		return comment;
+	}
+
+	public int lineNumber() {
+		return lineNumber;
 	}
 
 	public abstract void process(ItemProcessor processor);
