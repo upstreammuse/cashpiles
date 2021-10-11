fe9751c65f5fa6115eb89de069eb7745fa06404a
upstreammuse <upstreammuse@users.noreply.github.com>
2021-10-11 16:33:06 -0500

    Write AccountBalance to new ledger file

--- src/main/java/cashpiles/file/LedgerWriter.java
+++ src/main/java/cashpiles/file/LedgerWriter.java
@@ -1,7 +1,11 @@
 package cashpiles.file;
 
 import java.io.BufferedWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
+import cashpiles.ledger.AccountBalance;
 import cashpiles.ledger.ItemProcessor;
 
 public class LedgerWriter implements ItemProcessor {
@@ -10,6 +14,26 @@ public class LedgerWriter implements ItemProcessor {
 
 	public LedgerWriter(BufferedWriter writer) {
 		this.writer = writer;
+	}
+
+	@Override
+	public void process(AccountBalance balance) {
+		try {
+			writeDate(balance.date());
+			writer.write(" balance ");
+			writer.write(balance.account());
+			writer.write("  ");
+			writer.write(balance.amount().toString());
+			writer.newLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void writeDate(LocalDate date) throws IOException {
+		// TODO get this from the same place as the LedgerReader scanner
+		writer.write(date.format(DateTimeFormatter.ofPattern("M/d/yyyy")));
 	}
 
 }
