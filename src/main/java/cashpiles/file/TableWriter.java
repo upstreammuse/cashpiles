55e2ba9f279490ad31cebcd98a4a84150bd51b66
upstreammuse <upstreammuse@users.noreply.github.com>
2021-12-01 17:41:13 -0600

    Added support for right alignment in table output

--- src/main/java/cashpiles/file/TableWriter.java
+++ src/main/java/cashpiles/file/TableWriter.java
@@ -9,8 +9,17 @@ import cashpiles.util.Comparisons;
 
 class TableWriter {
 
-	private List<List<String>> cells = new ArrayList<>();
-	private List<String> currentRow;
+	public enum Alignment {
+		ALIGN_LEFT, ALIGN_RIGHT
+	}
+
+	private static class Cell {
+		Alignment align = Alignment.ALIGN_LEFT;
+		String value = "";
+	}
+
+	private List<List<Cell>> cells = new ArrayList<>();
+	private List<Cell> currentRow;
 	private List<Integer> maxWidths = new ArrayList<>();
 	private BufferedWriter writer;
 
@@ -27,7 +36,11 @@ class TableWriter {
 			var sb = new StringBuilder();
 			for (int i = 0; i < row.size(); i++) {
 				if (maxWidths.get(i) > 0) {
-					sb.append(String.format("%-" + maxWidths.get(i) + "s", row.get(i)));
+					String aligner = switch (row.get(i).align) {
+					case ALIGN_LEFT -> "-";
+					case ALIGN_RIGHT -> "";
+					};
+					sb.append(String.format("%" + aligner + maxWidths.get(i) + "s", row.get(i).value));
 				}
 			}
 			writer.write(sb.toString().stripTrailing());
@@ -45,21 +58,28 @@ class TableWriter {
 		currentRow = null;
 	}
 
+	void write(int index, String cell) {
+		write(index, cell, Alignment.ALIGN_LEFT);
+	}
+
 	// TODO there are almost certainly gotchas in here relating to unicode printable
 	// character count vs string length
-	void write(int index, String cell) {
+	public void write(int index, String content, Alignment align) {
 		if (currentRow == null) {
 			currentRow = new ArrayList<>();
 		}
 		while (currentRow.size() <= index) {
-			currentRow.add("");
+			currentRow.add(new Cell());
 		}
+		var cell = currentRow.get(index);
+		cell.align = align;
+		cell.value = content;
 		currentRow.set(index, cell);
 
 		while (maxWidths.size() <= index) {
 			maxWidths.add(0);
 		}
-		maxWidths.set(index, Comparisons.max(maxWidths.get(index), cell.length()));
+		maxWidths.set(index, Comparisons.max(maxWidths.get(index), content.length()));
 	}
 
 }
