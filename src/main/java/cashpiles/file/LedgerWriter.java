e96b8cc47bd2886544461ed620373c5e49278859
upstreammuse <upstreammuse@users.noreply.github.com>
2021-12-01 17:13:46 -0600

    Figured out a way to write table-aligned output in a fairly generic way

--- src/main/java/cashpiles/file/LedgerWriter.java
+++ src/main/java/cashpiles/file/LedgerWriter.java
@@ -8,11 +8,16 @@ import java.time.format.DateTimeFormatter;
 import cashpiles.ledger.AccountBalance;
 import cashpiles.ledger.AccountCommand;
 import cashpiles.ledger.BlankLine;
+import cashpiles.ledger.Budget;
+import cashpiles.ledger.CloseBudgetEntry;
+import cashpiles.ledger.IncomeBudgetEntry;
 import cashpiles.ledger.ItemProcessor;
 import cashpiles.ledger.LedgerItem;
+import cashpiles.ledger.RoutineBudgetEntry;
 
 public class LedgerWriter implements ItemProcessor {
 
+	private TableWriter table;
 	private BufferedWriter writer;
 
 	public LedgerWriter(BufferedWriter writer) {
@@ -22,6 +27,10 @@ public class LedgerWriter implements ItemProcessor {
 	@Override
 	public void process(AccountBalance balance) {
 		try {
+			if (table != null) {
+				table.flush();
+				table = null;
+			}
 			writeDate(balance.date());
 			writer.write(" balance ");
 			writer.write(balance.account());
@@ -29,15 +38,18 @@ public class LedgerWriter implements ItemProcessor {
 			writer.write(balance.amount().toString());
 			writeComment(balance);
 			writer.newLine();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (IOException ex) {
+			// TODO handle it
 		}
 	}
 
 	@Override
 	public void process(AccountCommand command) {
 		try {
+			if (table != null) {
+				table.flush();
+				table = null;
+			}
 			writeDate(command.date());
 			writer.write(" ");
 			writer.write(switch (command.status()) {
@@ -49,21 +61,93 @@ public class LedgerWriter implements ItemProcessor {
 			writer.write(command.account());
 			writeComment(command);
 			writer.newLine();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (IOException ex) {
+			// TODO handle it
 		}
 	}
 
 	@Override
 	public void process(BlankLine line) {
 		try {
+			if (table != null) {
+				table.flush();
+				table = null;
+			}
 			writeComment(line, false);
 			writer.newLine();
 		} catch (IOException ex) {
 			// TODO do something
-			ex.printStackTrace();
 		}
+	}
+
+	@Override
+	public boolean process(Budget budget) {
+		try {
+			if (table != null) {
+				table.flush();
+				table = null;
+			}
+			writeDate(budget.date());
+			writer.write(" budget ");
+			writer.write(budget.period().toString());
+			writeComment(budget);
+			writer.newLine();
+		} catch (IOException ex) {
+			// TODO handle it
+		}
+		return true;
+	}
+
+	@Override
+	public void process(CloseBudgetEntry entry) {
+		if (table == null) {
+			table = new TableWriter(writer);
+		}
+		table.write(0, "  close ");
+		table.write(1, entry.name());
+		table.write(10, formatComment(entry));
+		table.newLine();
+	}
+
+	@Override
+	public void process(IncomeBudgetEntry entry) {
+		if (table == null) {
+			table = new TableWriter(writer);
+		}
+		table.write(0, "  income ");
+		table.write(1, entry.name() + "  ");
+		table.write(2, entry.owner());
+		table.write(10, formatComment(entry));
+		table.newLine();
+	}
+
+	@Override
+	public void process(RoutineBudgetEntry entry) {
+		if (table == null) {
+			table = new TableWriter(writer);
+		}
+		table.write(0, "  routine ");
+		table.write(1, entry.name() + "  ");
+		table.write(2, entry.owner());
+		table.write(10, formatComment(entry));
+		table.newLine();
+	}
+
+	private String formatComment(LedgerItem item) {
+		return formatComment(item, true);
+	}
+
+	private String formatComment(LedgerItem item, boolean pad) {
+		var sb = new StringBuilder();
+		if (item.comment().isEmpty()) {
+			return sb.toString();
+		}
+		if (pad) {
+			sb.append(' ');
+		}
+		sb.append("; ");
+		sb.append(item.comment());
+		return sb.toString();
 	}
 
 	private void writeComment(LedgerItem item) throws IOException {
@@ -71,14 +155,7 @@ public class LedgerWriter implements ItemProcessor {
 	}
 
 	private void writeComment(LedgerItem item, boolean pad) throws IOException {
-		if (item.comment().isEmpty()) {
-			return;
-		}
-		if (pad) {
-			writer.write(" ");
-		}
-		writer.write("; ");
-		writer.write(item.comment());
+		writer.write(formatComment(item, pad));
 	}
 
 	private void writeDate(LocalDate date) throws IOException {
