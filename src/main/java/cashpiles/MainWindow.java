5cc32483ade8460834dec95ed55c7e918f41b12e
upstreammuse <upstreammuse@users.noreply.github.com>
2021-12-03 16:19:14 -0600

    Added a mock transaction panel to test notifications and live modifications

--- src/main/java/cashpiles/MainWindow.java
+++ src/main/java/cashpiles/MainWindow.java
@@ -31,9 +31,11 @@ import cashpiles.ledger.LedgerException;
 class MainWindow extends JFrame {
 
 	private final Ledger ledger;
+	private final TransactionPanel xactPanel;
 
 	MainWindow(Ledger ledger) {
 		this.ledger = ledger;
+		xactPanel = new TransactionPanel(ledger);
 		initUi();
 	}
 
@@ -87,6 +89,8 @@ class MainWindow extends JFrame {
 		var openButton = new JButton("Open...");
 		openButton.addActionListener(this::openFile);
 
+		layout.setVerticalGroup(layout.createParallelGroup().addComponent(openButton).addComponent(xactPanel));
+		layout.setHorizontalGroup(layout.createSequentialGroup().addComponent(openButton).addComponent(xactPanel));
 		pack();
 	}
 
