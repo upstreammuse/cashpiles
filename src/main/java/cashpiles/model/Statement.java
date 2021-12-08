ed0446672b8df5fa1cb559b46aa47910afedb11b
upstreammuse <upstreammuse@users.noreply.github.com>
2021-12-08 00:17:51 -0600

    Encapsulated some model logic better

--- src/main/java/cashpiles/model/Statement.java
+++ src/main/java/cashpiles/model/Statement.java
@@ -3,25 +3,63 @@ package cashpiles.model;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import cashpiles.currency.Amount;
+import cashpiles.ledger.AccountBalance;
 
-class Statement {
+class Statement extends ModelItem {
 
-	LocalDate closingDate;
-	final Amount startBalance;
-	final List<TransactionParticle> transactions = new ArrayList<>();
+	private Optional<LocalDate> closingDate = Optional.empty();
+	private final Amount startBalance;
+	private List<TransactionParticle> transactions = new ArrayList<>();
 
 	Statement(Amount startBalance) {
 		this.startBalance = startBalance;
 	}
 
 	Amount balance() {
-		var balance = startBalance;
-		for (var particle : transactions) {
-			balance = balance.add(particle.amount());
+		return transactions.stream().map(particle -> particle.amount()).reduce(startBalance,
+				(total, value) -> total.add(value));
+	}
+
+	Optional<LocalDate> closingDate() {
+		return closingDate;
+	}
+
+	Statement withReconciliation(AccountBalance balance) throws LedgerModelException {
+		var retval = clone();
+		retval.closingDate = Optional.of(balance.date());
+		retval.transactions.removeIf(x -> x.date().compareTo(balance.date()) > 0);
+		retval.transactions.removeIf(x -> switch (x.status()) {
+		case CLEARED -> false;
+		case DISPUTED -> false;
+		case PENDING -> true;
+		});
+		if (!retval.balance().equals(balance.amount())) {
+			throw LedgerModelException.forUnbalanced(balance, retval.balance());
 		}
-		return balance;
+		return retval;
+	}
+
+	Statement withReconciliationRemainder(Statement other) {
+		var retval = new Statement(other.balance());
+		retval.transactions = new ArrayList<>(transactions);
+		retval.transactions.removeAll(other.transactions);
+		return retval;
+	}
+
+	Statement withTransaction(TransactionParticle particle) {
+		var retval = clone();
+		retval.transactions.add(particle);
+		return retval;
+	}
+
+	@Override
+	public Statement clone() {
+		var retval = (Statement) super.clone();
+		retval.transactions = new ArrayList<>(transactions);
+		return retval;
 	}
 
 }
