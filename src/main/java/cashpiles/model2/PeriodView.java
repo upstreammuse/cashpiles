1d4ac7cfde439f334ea2175b20817ad8238722c1
upstreammuse <upstreammuse@users.noreply.github.com>
2022-06-19 17:45:38 -0500

    Additional view layer periods->period->categories

---
+++ src/main/java/cashpiles/model2/PeriodView.java
@@ -0,0 +1,18 @@
+package cashpiles.model2;
+
+import cashpiles.currency.Amount;
+import cashpiles.time.DateRange;
+
+public interface PeriodView {
+
+	Amount activity();
+
+	Amount allocation();
+
+	Amount balance();
+
+	CategoriesView categories();
+
+	DateRange dates();
+
+}
