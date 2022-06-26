0dad6a4406d7cae1a07c20798d423e03aa08478a
upstreammuse <upstreammuse@users.noreply.github.com>
2022-06-26 15:42:50 -0500

    Fix for bad LedgerItem comparison
    This showed up as an inability to reconcile transactions.  Making transactions conform to the immutable data class paradigm caused the rest of the program to no longer correctly consider the 'same' transactions as equivalent, which broke logic that makes such comparisons.  Now that LI has a unique persistent id, those kinds of issues should disappear.

--- src/main/java/cashpiles/ledger/LedgerItem.java
+++ src/main/java/cashpiles/ledger/LedgerItem.java
@@ -1,10 +1,13 @@
 package cashpiles.ledger;
 
+import java.util.UUID;
+
 // TODO the fileName field is not used by anything, and can be removed if there's no use for it going forward
 public abstract class LedgerItem implements Cloneable {
 
 	private final String comment;
 	private final String fileName;
+	private final UUID id = UUID.randomUUID();
 	private final int lineNumber;
 
 	public LedgerItem(String fileName, int lineNumber, String comment) {
@@ -17,8 +20,21 @@ public abstract class LedgerItem implements Cloneable {
 		return comment;
 	}
 
+	@Override
+	public boolean equals(Object other) {
+		if (other == null || !(other instanceof LedgerItem)) {
+			return false;
+		}
+		return id.equals(((LedgerItem) other).id);
+	}
+
 	public String fileName() {
 		return fileName;
+	}
+
+	@Override
+	public int hashCode() {
+		return id.hashCode();
 	}
 
 	public int lineNumber() {
