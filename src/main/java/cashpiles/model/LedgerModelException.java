acd6a8084479f42bfa8851cef8d2050332738721
upstreammuse <upstreammuse@users.noreply.github.com>
2022-09-13 21:24:57 -0500

    Removed redundant exception

--- src/main/java/cashpiles/model/LedgerModelException.java
+++ src/main/java/cashpiles/model/LedgerModelException.java
@@ -1,17 +1,10 @@
 package cashpiles.model;
 
-import cashpiles.currency.Amount;
-import cashpiles.ledger.AccountBalance;
 import cashpiles.ledger.LedgerException;
 import cashpiles.ledger.LedgerItem;
 
 @SuppressWarnings("serial")
 public class LedgerModelException extends LedgerException {
-
-	static LedgerModelException forUnbalanced(AccountBalance balance, Amount calculated) {
-		return new LedgerModelException(balance,
-				"Statement balance " + balance.amount() + " and calculated balance " + calculated + " do not match.");
-	}
 
 	protected LedgerModelException(LedgerItem item, String message) {
 		super(item, message);
